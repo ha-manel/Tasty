cdac736343c3f10eabe6241c0fb177d2c0ea84ca
Manel <manelhammouche3@gmail.com>
2022-05-17 14:08:30 +0100

    Add ID to the cards

--- src/display-cards.js
+++ src/display-cards.js
@@ -1,14 +1,15 @@
 const recipeCards = document.querySelector('#recipes-cards');
 
-const createCard = (img, name) => {
+const createCard = (img, name, id) => {
   const card = document.createElement('div');
   card.className = 'card';
+  card.id = id;
   card.innerHTML = `<div class="meal-thumbnail">
         <img src="${img}" alt="">
       </div>
       <div class="card-header">
         <h3 class="meal-name">${name}</h3>
-        <button class="like-btn"><i class="fa-regular fa-heart"></i> 201</button>
+        <button class="like-btn"><i class="fa-regular fa-heart"></i> <span class="likes-count">0</span></button>
       </div>
       <button class="comments-btn">Comments</button>`;
 
@@ -20,7 +21,7 @@ const fetchRecipes = async () => {
     .then((response) => response.json())
     .then((json) => {
       json.meals.forEach((meal) => {
-        createCard(meal.strMealThumb, meal.strMeal);
+        createCard(meal.strMealThumb, meal.strMeal, meal.idMeal);
       });
     });
 };
