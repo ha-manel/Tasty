c10a6ac9234912e5a6591ac8bb839acc2c22837a
leonard33 <leonardkombo14@gmail.com>
2022-05-17 06:38:30 +0300

    add popup modal layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './style.css';
+
+const btnComment = document.querySelector('.comments-btn');
+const popupmodal = document.querySelector('.modal');
+const closepopup = document.querySelector('.close');
+
+btnComment.addEventListener('click', (e) => {
+  if (e.target) {
+    popupmodal.style.display = 'block';
+  }
+});
+
+closepopup.addEventListener('click', (e) => {
+  if (e.target) {
+    popupmodal.style.display = 'none';
+  }
+});
