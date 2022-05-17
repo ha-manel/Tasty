ecc7ca975e600b1fa3c04ec24b523b15bb616377
Manel <manelhammouche3@gmail.com>
2022-05-17 17:59:34 +0100

    Make card modal window

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import './style.css';
 import fetchRecipes from './display-cards.js';
 import { getLikes, postLike } from './like-item.js';
+import displayPopup from './popup-modal.js';
 
 const display = async () => {
   await fetchRecipes();
-  await postLike();
   await getLikes();
+  await displayPopup();
+  await postLike();
 };
 
 display();
---
+++ src/popup-modal.js
@@ -0,0 +1,27 @@
+const modal = document.querySelector('.comment-modal');
+const modalImg = document.querySelector('.popup-image img');
+const modalTitle = document.querySelector('.item-info h3');
+const modalRecipe = document.querySelector('.recipe');
+
+const baseURL = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
+
+const displayPopup = () => {
+  const cards = document.querySelectorAll('.card');
+  const commentBtns = document.querySelectorAll('.comments-btn');
+
+  commentBtns.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      fetch(`${baseURL}${cards[index].id}`)
+        .then((response) => response.json())
+        .then((json) => {
+          modal.classList.add('active');
+          document.querySelector('body').classList.add('no-scroll');
+          modalImg.setAttribute('src', json.meals[0].strMealThumb);
+          modalTitle.innerHTML = json.meals[0].strMeal;
+          modalRecipe.innerHTML = json.meals[0].strInstructions;
+        });
+    });
+  });
+};
+
+export default displayPopup;
