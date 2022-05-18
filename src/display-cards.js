7cb889458047a962075bd22890db62c49672ccd5
leonard33 <leonardkombo14@gmail.com>
2022-05-18 06:07:06 +0300

    Add a popup modal

--- src/display-cards.js
+++ src/display-cards.js
@@ -1,6 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+import openmodal from './popup.js';
+
 const recipeCards = document.querySelector('#recipes-cards');
 
-const createCard = (img, name) => {
+const createCard = (img, name, id) => {
   const card = document.createElement('div');
   card.className = 'card';
   card.innerHTML = `<div class="meal-thumbnail">
@@ -10,7 +13,7 @@ const createCard = (img, name) => {
         <h3 class="meal-name">${name}</h3>
         <button class="like-btn"><i class="fa-regular fa-heart"></i> 201</button>
       </div>
-      <button class="comments-btn">Comments</button>`;
+      <button class="comments-btn" id="comments-btn" onclick="openmodal(${id})">Comments</button>`;
 
   recipeCards.appendChild(card);
 };
@@ -20,7 +23,7 @@ const fetchRecipes = async () => {
     .then((response) => response.json())
     .then((json) => {
       json.meals.forEach((meal) => {
-        createCard(meal.strMealThumb, meal.strMeal);
+        createCard(meal.strMealThumb, meal.strMeal, meal.idMeal);
       });
     });
 };
