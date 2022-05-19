82d00f49ab610adddba721ccbd10557fb525e333
leonard33 <leonardkombo14@gmail.com>
2022-05-19 15:52:58 +0300

    Add id to comments buttons

--- src/display-cards.js
+++ src/display-cards.js
@@ -1,19 +1,17 @@
-// eslint-disable-next-line no-unused-vars
-import openmodal from './popup.js';
-
 const recipeCards = document.querySelector('#recipes-cards');
 
 const createCard = (img, name, id) => {
   const card = document.createElement('div');
   card.className = 'card';
+  card.id = id;
   card.innerHTML = `<div class="meal-thumbnail">
         <img src="${img}" alt="">
       </div>
       <div class="card-header">
         <h3 class="meal-name">${name}</h3>
-        <button class="like-btn"><i class="fa-regular fa-heart"></i> 201</button>
+        <button class="like-btn"><i class="fa-regular fa-heart"></i> <span class="likes-count">0</span></button>
       </div>
-      <button class="comments-btn" id="comments-btn" onclick="openmodal(${id})">Comments</button>`;
+      <button class="comments-btn" id="${id}">Comments</button>`;
 
   recipeCards.appendChild(card);
 };
